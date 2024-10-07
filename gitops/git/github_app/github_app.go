d724d6397625a5cc0cef841b4c9a735e064535b3
Naz Karnasevych <nkarnasevych@etsy.com>
2024-10-07 13:08:33 -0400

    small changes

--- gitops/git/github_app/github_app.go
+++ gitops/git/github_app/github_app.go
@@ -9,28 +9,27 @@ import (
 	"net/http"
 
 	"github.com/bradleyfalzon/ghinstallation/v2"
-	"github.com/google/go-github/v58/github"
+	"github.com/google/go-github/v63/github"
 )
 
 var (
-	git                     = "git"
-	repoOwner               = flag.String("github_app_repo_owner", "", "the owner user/organization to use for github api requests")
-	repo                    = flag.String("github_app_repo", "", "the repo to use for github api requests")
-	githubEnterpriseHost    = flag.String("github_app_enterprise_host", "", "The host name of the private enterprise github, e.g. git.corp.adobe.com")
 	message                 = flag.String("message", "", "Message to send")
 	privateKey              = flag.String("private_key", "/var/run/agent-secrets/buildkite-agent/secrets/github-pr-creator-key", "Private Key")
-	gitHubAppId             = flag.Int64("github_app_id", 257131, "GitHub App Id")
+	gitHubAppId             = flag.Int64("github_app_id", 1014336, "GitHub App Id")
 	gitHubAppInstallationId = flag.Int64("github_installation_id", 30831292, "GitHub App Id")
 	gitHubUser              = flag.String("github_user", "etsy", "GitHub User")
-	gitHubAppName           = flag.String("github_app_name", "", "Name of the GitHub App")
+	gitHubAppName           = flag.String("github_app_name", "gitops-pr-creator", "Name of the GitHub App")
 )
 
 func CreatePR(from, to, title, body string) error {
 	if *repoOwner == "" {
-		return errors.New("github_app_repo_owner must be set")
+		return errors.New("github_repo_owner must be set")
 	}
 	if *repo == "" {
-		return errors.New("github_app_repo must be set")
+		return errors.New("github_repo must be set")
+	}
+	if *gitHubAppId == "" {
+		return errors.New("github_app_id must be set")
 	}
 
 	ctx := context.Background()
